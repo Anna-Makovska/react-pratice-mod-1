ecbe4c7880fdc11f8e61399d657ea30d7f5c3230
Brain-Lady <brain.lady404@gmail.com>
2025-04-25 17:04:19 +0300

    done

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,17 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import style from './StatisticsItem.module.css'
+import { IconContext } from 'react-icons';
+const StatisticsItem = ({title, total, icon}) => {
+  return (
+    <>
+      <IconContext.Provider
+        value={{ color: 'black', size: '40' }}
+      >
+        {icon}
+      </IconContext.Provider>
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
 
 export default StatisticsItem;
